5f06027f5f3d6398522744749eeb0a271faf1378
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2024-07-10 19:30:09 +0000

    deleted

--- sum.js
+++
@@ -1,7 +0,0 @@
-var sum=0;
-
-for(let i=0;i<=100;i++){
-    sum=sum+i;
-}
-
-console.log(sum);
--- tailwind/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dict/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
